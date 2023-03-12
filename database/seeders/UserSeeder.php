3cac7af8130350d67e4a6fddae695c507b3b998e
Julio Blanco Windows <juliocesarbr2591@gmail.com>
2023-03-12 18:03:30 -0500

    Added spatie/permissions library.

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'name' => 'Admin',
+                'email' => 'admin@migranthelper.com',
+                'password' => 'password',
+                'role' => 'admin',
+            ],
+            [
+                'name' => 'Test Migrant',
+                'email' => 'test@migranthelper.com',
+                'password' => 'password2',
+                'role' => 'migrant',
+            ]
+        ];
+
+        foreach($users as $user) {
+            $created_user = User::create([
+                'name' => $user['name'],
+                'email' => $user['email'],
+                'password' => Hash::make($user['password']),
+            ]);
+
+            $created_user->assignRole($user['role']);
+        }
+    }
+}
