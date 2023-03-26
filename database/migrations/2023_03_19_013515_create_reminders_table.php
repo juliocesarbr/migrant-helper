6fc925ecf68ff429b4818837fcef87196066f324
Julio Blanco Windows <juliocesarbr2591@gmail.com>
2023-03-26 07:32:13 -0500

    Add migrations and seeders

---
+++ database/migrations/2023_03_19_013515_create_reminders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reminders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->timestamp('remind_at');
+            $table->boolean('is_complete')->default(false);
+            $table->string('entity_type');
+            $table->unsignedBigInteger('entity_id');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reminders');
+    }
+};
